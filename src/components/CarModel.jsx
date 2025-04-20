e10d811fab8bfbfde99f50044f9828b60ac3c268
Rida-Lad <ridlad7@gmail.com>
2025-04-20 16:01:44 +0100

    feat: enhance CarModel component with improved lighting and canvas styling

--- src/components/CarModel.jsx
+++ src/components/CarModel.jsx
@@ -20,11 +20,16 @@ const CarModel = () => {
         <div>
             <h1>Car Model</h1>
             <p>Welcome to the Car Model page. Here you can find details about various car models available for rent.</p>
-            <Canvas style={{ height: '500px' }}>
-                <ambientLight intensity={0.5} />
-                <directionalLight position={[10, 10, 5]} />
+            <Canvas>
+                <ambientLight intensity={1} />
+                <directionalLight position={[10, 10, 5]} intensity={1.5} />
+                <hemisphereLight intensity={0.6} groundColor="white" />
+                <OrbitControls 
+                    enableZoom={false} 
+                    maxPolarAngle={Math.PI / 2} 
+                    minPolarAngle={Math.PI / 2} 
+                />
                 <Car3DModel />
-                <OrbitControls />
             </Canvas>
         </div>
     );
