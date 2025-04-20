0ce3f37eabd52b617b6b18e2ecbab5486dc804ac
Rida-Lad <ridlad7@gmail.com>
2025-04-20 15:42:59 +0100

    feat: add CarModel component for displaying car details

---
+++ src/components/CarModel.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CarModel = () => {
+    return (
+        <div>
+            <h1>Car Model</h1>
+            <p>Welcome to the Car Model page. Here you can find details about various car models available for rent.</p>
+        </div>
+    );
+};
+
+export default CarModel;
