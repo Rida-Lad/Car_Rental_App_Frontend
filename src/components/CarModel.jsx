8689d074101a19d459ed329eaad7312c9bcb4566
Rida-Lad <ridlad7@gmail.com>
2025-04-20 16:02:54 +0100

    feat: remove unnecessary lighting from Car3DModel for simplified rendering

--- src/components/CarModel.jsx
+++ src/components/CarModel.jsx
@@ -6,13 +6,7 @@ import { OrbitControls, useGLTF } from '@react-three/drei';
 
 const Car3DModel = () => {
     const { scene } = useGLTF('/car-model.glb');
-    return (
-        <>
-            <ambientLight intensity={0.8} />
-            <spotLight position={[10, 15, 10]} angle={0.3} penumbra={1} intensity={1} castShadow />
-            <primitive object={scene} scale={0.5} />
-        </>
-    );
+    return <primitive object={scene} scale={0.5} />;
 };
 
 const CarModel = () => {
