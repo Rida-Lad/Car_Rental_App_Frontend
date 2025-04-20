3799356ded034dbce2caf005d8528dc203173d56
Rida-Lad <ridlad7@gmail.com>
2025-04-20 15:53:25 +0100

    feat: create Auth component for user authentication

---
+++ src/pages/Auth.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Auth = () => {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>Welcome to Car Rental</h1>
+            <p>Please sign in or register to continue.</p>
+        </div>
+    );
+};
+
+export default Auth;
