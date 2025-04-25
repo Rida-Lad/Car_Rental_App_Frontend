25a3061933beba753023baebaa7636a897871649
Rida-Lad <ridlad7@gmail.com>
2025-04-25 21:20:30 +0100

    feat: remove CarModel and Auth components to simplify App structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import CarModel from './components/CarModel';
-import Auth from './pages/Auth';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Car Model App</h1>
-      </header>
-      <main>
-        <CarModel />
-        <Auth />
-      </main>
-    </div>
+    <>
+    </>
   );
 }
 
