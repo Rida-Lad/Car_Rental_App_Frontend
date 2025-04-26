851854c525d8296d71c27d063946685dccbfa901
Rida-Lad <ridlad7@gmail.com>
2025-04-26 21:29:39 +0100

    refactor: remove unused components and routes for a cleaner application structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AddCar from './pages/AddCar';
 import CarsList from './pages/CarsList';
-import CarDetails from './pages/CarDetails';
-import Auth from './pages/Auth';
-import ProtectedRoute from './components/ProtectedRoute';
-
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<CarsList />} />
-        <Route path="/cardetails/:id" element={
-          <ProtectedRoute>
-            <CarDetails />
-          </ProtectedRoute>
-        } />
-        <Route path="/auth" element={<Auth />} />
         <Route path="/admin/addcar" element={<AddCar />} />
       </Routes>
     </Router>
