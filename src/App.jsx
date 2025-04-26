6ecc7de5f975a31eaaf73c105edf2c36c2a22009
Rida-Lad <ridlad7@gmail.com>
2025-04-26 16:10:52 +0100

    feat: remove routing and authentication logic from App component for simplification

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,9 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-import Home from './pages/Home';
-import CarDetails from './pages/CarDetails';
-import Auth from './pages/Auth';
-import MyOrders from './pages/MyOrders';
-import AdminAddCar from './pages/AdminAddCar';
-import AdminSetAvailable from './pages/AdminSetAvailable';
-import AdminOrders from './pages/AdminOrders';
-import ProtectedRoute from './components/ProtectedRoute';
-
+import React from 'react';
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/auth" element={<Auth />} />
-          
-          <Route element={<ProtectedRoute />}>
-            <Route path="/car/:id" element={<CarDetails />} />
-            <Route path="/myorders" element={<MyOrders />} />
-          </Route>
-
-          <Route element={<ProtectedRoute adminOnly={true} />}>
-            <Route path="/admin/addcar" element={<AdminAddCar />} />
-            <Route path="/admin/setavailable" element={<AdminSetAvailable />} />
-            <Route path="/admin/orders" element={<AdminOrders />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+    <div>
+      <h1>Welcome to My App</h1>
+      <p>This is a simple paragraph.</p>
+    </div>
   );
 }
