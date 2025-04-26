6aa4767489208da478796a24f0bb70aa7b72829e
Rida-Lad <ridlad7@gmail.com>
2025-04-26 22:09:06 +0100

    TESTING

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,16 @@
+// src/components/ProtectedRoute.jsx
+import { Navigate, useLocation } from 'react-router-dom';
+
+const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem('token');
+  const location = useLocation();
+
+  if (!token) {
+    return <Navigate to="/auth" state={{ from: location }} replace />;
+  }
+
+  return children;
+};
+
+
+export default ProtectedRoute;
